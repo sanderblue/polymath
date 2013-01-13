ce615e4d5d98cd5ec430fbf430d8f0748e29e626
sanderblue <william.a.blue@gmail.com>
2013-01-12 23:45:54 -0600

    Added to algebra

--- src/Polymath/Geometry.php
+++ src/Polymath/Geometry.php
@@ -8,4 +8,19 @@ class Geometry
     {
         return 2 * $x + 2 * $y;
     }
+
+    public function areaSquare($x, $y)
+    {
+        return $x * $y;
+    }
+
+    public function areaRectangle($x, $y)
+    {
+        return $x * $y;
+    }
+
+    public function areaCircle($r)
+    {
+        return pi() * $r * $r;
+    }
 }
--- tests/Polymath/Test/GeometryTest.php
+++ tests/Polymath/Test/GeometryTest.php
@@ -24,4 +24,61 @@ class GeometryTest extends PHPUnit_Framework_TestCase
             array(3, 5, 16)
         ); 
     }
+
+    /**
+     * @dataProvider getTestAreaSquareData
+     **/
+    public function testAreaSquare($x, $y, $expected)
+    {
+        $geometry = new \Polymath\Geometry();
+        $result = $geometry->areaSquare($x, $y);
+        $this->assertEquals($result, $expected, 'Given x and y, assert the perimeter of a rectangle'); 
+    }
+
+    public function getTestAreaSquareData()
+    {
+        return array(
+            array(1, 1, 1),
+            array(2, 2, 4),
+            array(3, 3, 9)
+        ); 
+    }
+
+    /**
+     * @dataProvider getTestAreaRectangleData
+     **/
+    public function testAreaRectangle($x, $y, $expected)
+    {
+        $geometry = new \Polymath\Geometry();
+        $result = $geometry->areaRectangle($x, $y);
+        $this->assertEquals($result, $expected, 'Given x and y, assert the perimeter of a rectangle'); 
+    }
+
+    public function getTestAreaRectangleData()
+    {
+        return array(
+            array(1, 1, 1),
+            array(2, 3, 6),
+            array(3, 6, 18)
+        ); 
+    }
+
+    /**
+     * @dataProvider getTestAreaCircleData
+     **/
+    public function testAreaCircle($r, $expected)
+    {
+        $geometry = new \Polymath\Geometry();
+        $result = $geometry->areaCircle($r, pi() * $r * $r);
+        $this->assertEquals($result, $expected, 'Assert the area of a circle with r radius'); 
+    }
+
+    public function getTestAreaCircleData()
+    {
+        // With a given radius, assert the area of a cirle
+        return array(
+            array(1, 3.1415926535898),
+            array(2, 12.5663706144)
+        ); 
+    }
 }
