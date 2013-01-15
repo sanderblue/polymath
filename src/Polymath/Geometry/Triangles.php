56aba5ff16cdde276763e699b5d0c89fb87974b5
sanderblue <william.a.blue@gmail.com>
2013-01-15 14:42:55 -0600

    Added to triangles, cleaned up code and unit test

--- src/Polymath/Geometry/Triangles.php
+++ src/Polymath/Geometry/Triangles.php
@@ -15,4 +15,16 @@ class Triangles
     {
         return 180 - ($a + $b); 
     }
+
+    /**
+     * Calculate the length of the hypotenuse of a right-angle triangle
+     *
+     * @param integer $a
+     * @param integer $b
+     * @return integer $c
+     **/
+    public function hypotRightTriangle($a, $b)
+    {
+        return hypot($a, $b); 
+    }
 }
--- tests/Polymath/Test/Geometry/TrianglesTest.php
+++ tests/Polymath/Test/Geometry/TrianglesTest.php
@@ -12,8 +12,9 @@ class TrianglesTest extends PHPUnit_Framework_TestCase
      */
     public function testCalculateInteriorAngle($a, $b, $c)
     {
-        $triangles = new Triangles();
-        $this->assertEquals($c, $triangles->calculateInteriorAngle($a, $b));
+        $triangles = new \Polymath\Geometry\Triangles();
+        $result = $triangles->calculateInteriorAngle($a, $b);
+        $this->assertEquals($result, $c, 'Assert value of the missing angle of a triangle given angles a and b of a');
     }
 
     public function getTestCalculateAngleData()
@@ -22,6 +23,25 @@ class TrianglesTest extends PHPUnit_Framework_TestCase
             array(100, 40, 40),
             array(50, 50, 80),
             array(20, 80, 80)
+        );
+    }
+
+    /**
+     * @dataProvider getTestHypotRightTriangleData
+     */
+    public function testHypotRightTriangleAngle($a, $b, $c)
+    {
+        $triangles = new \Polymath\Geometry\Triangles();
+        $result = $triangles->hypotRightTriangle($a, $b);
+        $this->assertEquals($result, $c, 'Assert value of the hypotenuse of a right triangle given sides a and b');
+    }
+
+    public function getTestHypotRightTriangleData()
+    {
+        return array(
+            array(1, 2, 2.2360679774998),
+            array(3, 4, 5.0),
+            array(5, 6, 7.8102496759067)
         );
     }
 }
