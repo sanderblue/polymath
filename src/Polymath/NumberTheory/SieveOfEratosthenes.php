15ce25189ec6d8d30f2009c66ae42392caa986bc
sanderblue <william.a.blue@gmail.com>
2013-01-16 20:53:44 -0600

    Added NumberTheory directory, added Sieve of Eratosthenes for prime numbers

---
+++ src/Polymath/NumberTheory/SieveOfEratosthenes.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Polymath\NumberTheory;
+
+class SieveOfEratosthenes 
+{
+    /**
+     * Prime Numbers (my favorite)
+     *
+     * This class uses the sieve of Eratosthenes to find prime numbers
+     *
+     * The National Institute of Standards and Technology (NIST) defines the Sieve of Eratosthenes as follows:
+     *
+     * Definition: An algorithm to find all prime numbers up to a certain N. 
+     * Begin with an (unmarked) array of integers from 2 to N. The first unmarked integer, 2, 
+     * is the first prime. Mark every multiple of this prime. Repeatedly take 
+     * the next unmarked integer as the next prime and mark every multiple of the prime.
+     *
+     * Ported from their old Java code (thanks NIST)
+     * @see http://xlinux.nist.gov/dads/HTML/sieve.html
+     */
+    public function getPrimes($upTo) 
+    {
+        if ($upTo > 0) {
+        $size   = $upTo + 1;
+        $flags  = array();
+        $primes = array();
+        $limit  = sqrt($size);
+  
+        // Set flags to start off
+        for ($i = 2; $i < $size; $i++) 
+            $flags[$i] = true;
+  
+        // Cross out multiples of 2
+        $j = 2;
+        for ($i = $j + $j; $i < $size; $i = $i + $j) 
+            $flags[$i] = false;
+  
+        // Cross out multiples of odd numbers
+        for ($j = 3; $j <= $limit; $j = $j + 2) {
+            if ($flags[$j]) {
+                for ($i = $j + $j; $i < $size; $i = $i + $j) 
+            $flags[$i] = false;
+            }
+        }
+  
+       // Build list of primes from what is left
+       for ($i = 2; $i < $size; $i++) {
+         if ($flags[$i]) 
+           $primes[] = $i;
+        }
+  
+        return $primes;
+
+    } else     
+      die("n must be greater than 0."); 
+    }
+}
---
+++ tests/Polymath/Test/NumberTheory/SieveOfEratosthenes.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Polymath\NumberTheory\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class SieveOfEratosthenesTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @author Still needs proper unit test, but this works for now
+     *
+     * @dataProvider getTestSieveOfEratosthenesData
+     **/
+    public function testSieveOfEratosthenes()
+    {
+        $sieve  = new \Polymath\NumberTheory\SieveOfEratosthenes();
+        $primes = $sieve->getPrimes(420); // Will return all prime numbers up to 420
+        return $primes;
+    }
+}
